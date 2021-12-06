3be2479d94e5a40802da1c08d08ff803add77d21
SaidSuyv <playqlero@gmail.com>
2021-12-05 22:03:13 -0500

    v1.4.21

--- docs/script/showing_avialables_days.js
+++ docs/script/showing_avialables_days.js
@@ -25,32 +25,29 @@ function analizing(id,value){
 }
 
 function send_days(limites){
-//new Date(parseInt(limitDates[0]),parseInt(limitDates[1]) - 1,parseInt(limitDates[2]))
-
-  let strt = limites['start'];
-  let dateStart = strt[2];
+  let dateStart = limites['start'][2];
   let dayAlreadyWrote = false;
 
-  while(new Date(parseInt(strt[0]),parseInt(strt[1]) - 1,parseInt(dateStart)).getDate() <= parseInt(limites['end'][2])){
+  while(new Date(parseInt(limites['start'][0]),parseInt(limites['start'][1]) - 1,parseInt(dateStart)).toLocaleDateString() != new Date(parseInt(limites['end'][0]),parseInt(limites['end'][1])-1,parseInt(limites['end'][2])+1).toLocaleDateString()){
 
-    let currentMonth = new Date(parseInt(strt[0]),parseInt(strt[1]) - 1,parseInt(dateStart)).toLocaleDateString("en-US",{month:'long'});
+    let currentMonth = new Date(parseInt(limites['start'][0]),parseInt(limites['start'][1]) - 1,parseInt(dateStart)).toLocaleDateString("en-US",{month:'long'});
 
     let day_htmlElement = `
     <div class="av-day my-3 container d-flex justify-content-around align-items-center flex-wrap gy-3">
 
       <div class="form-check form-switch">
-        <input class="form-check-input" type="checkbox" role="switch" id="${currentMonth}${new Date(parseInt(strt[0]),parseInt(strt[1])-1,dateStart).getDate()}" name='{"year":"${new Date(parseInt(strt[0]),parseInt(strt[1])-1,dateStart).getFullYear()}","month":"${currentMonth}","date":"${new Date(parseInt(strt[0]),parseInt(strt[1])-1,dateStart).getDate()}"}' checked>
-        <label class="form-check-label" for="${currentMonth}${new Date(parseInt(strt[0]),parseInt(strt[1])-1,dateStart).getDate()}">${currentMonth} ${new Date(parseInt(strt[0]),parseInt(strt[1])-1,dateStart).getDate()}</label>
+        <input class="form-check-input av-day-inp" type="checkbox" role="switch" id="${currentMonth}${new Date(parseInt(limites['start'][0]),parseInt(limites['start'][1])-1,dateStart).getDate()}" name='{"year":"${new Date(parseInt(limites['start'][0]),parseInt(limites['start'][1])-1,dateStart).getFullYear()}","month":"${currentMonth}","date":"${new Date(parseInt(limites['start'][0]),parseInt(limites['start'][1])-1,dateStart).getDate()}"}' checked>
+        <label class="form-check-label" for="${currentMonth}${new Date(parseInt(limites['start'][0]),parseInt(limites['start'][1])-1,dateStart).getDate()}">${currentMonth} ${new Date(parseInt(limites['start'][0]),parseInt(limites['start'][1])-1,dateStart).getDate()}</label>
       </div>
 
       <div class="hours-inp d-flex gx-3 align-items-center">
 
         <div>
-          <input type="time" class="form-time-input" id="${currentMonth}${new Date(parseInt(strt[0]),parseInt(strt[1])-1,dateStart).getDate()}_hourStart" name="${currentMonth}${new Date(parseInt(strt[0]),parseInt(strt[1])-1,dateStart).getDate()}_hourStart">
+          <input type="time" class="form-time-input" id="${currentMonth}${new Date(parseInt(limites['start'][0]),parseInt(limites['start'][1])-1,dateStart).getDate()}_hourStart" name="${currentMonth}${new Date(parseInt(limites['start'][0]),parseInt(limites['start'][1])-1,dateStart).getDate()}_hourStart">
         </div>
         <div class="mx-3"><p class="m-auto">-</p></div>
         <div>
-          <input type="time" class="form-time-input" id="${currentMonth}${new Date(parseInt(strt[0]),parseInt(strt[1])-1,dateStart).getDate()}_hourEnd" name="${currentMonth}${new Date(parseInt(strt[0]),parseInt(strt[1])-1,dateStart).getDate()}_hourEnd">
+          <input type="time" class="form-time-input" id="${currentMonth}${new Date(parseInt(limites['start'][0]),parseInt(limites['start'][1])-1,dateStart).getDate()}_hourEnd" name="${currentMonth}${new Date(parseInt(limites['start'][0]),parseInt(limites['start'][1])-1,dateStart).getDate()}_hourEnd">
         </div>
 
       </div>
@@ -63,8 +60,16 @@ function send_days(limites){
       dayAlreadyWrote = true;
     }else document.querySelector('.available-days').innerHTML += day_htmlElement;
     dateStart++;
-    console.log('works');
   }
-  console.log('dont works or finished');
-
 }
+
+//-----------------------------------
+
+document.querySelectorAll('.loop').forEach(inp=>{
+  inp.onclick = ()=>{
+    if (!inp.checked){
+      var free_days = document.querySelectorAll('input.av-day-inp');
+      console.log(free_days);
+    }
+  };
+});
